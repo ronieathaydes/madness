7e6508b5d92ad36285c14ca48bce5d9edd6f5f67
Ronie Athaydes <ronieathaydes@gmail.com>
2021-04-30 22:19:15 -0300

    First commit

---
+++ app/src/main/java/com/madness/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.madness
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.madness.ui.theme.MadnessTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MadnessTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(color = MaterialTheme.colors.background) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    MadnessTheme {
+        Greeting("Android")
+    }
+}
