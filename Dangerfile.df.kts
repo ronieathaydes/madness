5abb0c9ebedaa1eaf6dabb520f4546b5bebe979f
Ronie Athaydes <ronieathaydes@gmail.com>
2021-05-05 19:35:59 -0300

    Add danger to pull request (#2)
    * Add danger to pull request
    
    * Update greeting message
    
    * Split greeting message
    
    * Add wip, assignees and merge validations
    
    * Update multiline validation messages

---
+++ Dangerfile.df.kts
@@ -0,0 +1,28 @@
+import systems.danger.kotlin.*
+
+danger(args) {
+
+    onGitHub {
+        message("Thank you so much for your work here @${pullRequest.user.login} 🎉 \nYou might find a few suggestions from me for this Pull Request below 🙂")
+
+        if (pullRequest.title.contains("WIP", false)) {
+            fail("I noticed this is marked as Work In Progress, does it need to be open before it is ready? 🤔 \nIf so, convert it to a Draft Pull Request and remove WIP from title 😉")
+        }
+
+        if (pullRequest.body.isEmpty()) {
+            warn("You should provide a summary in the description so that the reviewer has more context on this Pull Request 💚")
+        }
+
+        if (pullRequest.assignees.isEmpty()) {
+            fail("This Pull Request does not have any assignees yet 🧐")
+        }
+
+        if (commits.any { githubCommit -> githubCommit.commit.message.startsWith("Merge branch '${pullRequest.base.ref}'") }) {
+            fail("Please rebase to get rid of the merge commits in this Pull Request 🙃")
+        }
+
+        if ((pullRequest.additions ?: 0) - (pullRequest.deletions ?: 0) > 300) {
+            warn("This Pull Request is quite a big one! Maybe try splitting this into separate tasks next time 😅")
+        }
+    }
+}
