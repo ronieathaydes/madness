6bf77e2d7ad4f488a5aa665dfc6ab26eba770c71
Ronie Athaydes <ronieathaydes@gmail.com>
2021-05-05 19:01:51 -0300

    Update multiline validation messages

--- Dangerfile.df.kts
+++ Dangerfile.df.kts
@@ -3,10 +3,10 @@ import systems.danger.kotlin.*
 danger(args) {
 
     onGitHub {
-        message("Thank you so much for your work here @${pullRequest.user.login} 🎉 You might find a few suggestions from me for this Pull Request below 🙂")
+        message("Thank you so much for your work here @${pullRequest.user.login} 🎉 \nYou might find a few suggestions from me for this Pull Request below 🙂")
 
         if (pullRequest.title.contains("WIP", false)) {
-            fail("I noticed this is marked as Work In Progress, does it need to be open before it is ready? 🤔 If so, convert it to a Draft Pull Request and remove WIP from title 😉")
+            fail("I noticed this is marked as Work In Progress, does it need to be open before it is ready? 🤔 \nIf so, convert it to a Draft Pull Request and remove WIP from title 😉")
         }
 
         if (pullRequest.body.isEmpty()) {
