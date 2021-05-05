7968a06ecd93aa2dc6fdb04adde1415f6f246aa4
Ronie Athaydes <ronieathaydes@gmail.com>
2021-05-05 14:58:58 -0300

    Split greeting message

--- Dangerfile.df.kts
+++ Dangerfile.df.kts
@@ -3,7 +3,8 @@ import systems.danger.kotlin.*
 danger(args) {
 
     onGitHub {
-        message("Thank you so much for your work here @${pullRequest.user.login} 🎉\nYou might find a few suggestions from me for this Pull Request below 🙂")
+        message("Thank you so much for your work here @${pullRequest.user.login} 🎉")
+        message("You might find a few suggestions from me for this Pull Request below 🙂")
 
         if (pullRequest.body.isEmpty()) {
             warn("You should provide a summary in the Pull Request description so that the reviewer has more context on this Pull Request 💚")
