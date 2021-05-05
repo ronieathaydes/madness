32578926afd76a0d1b88ebd962dc852517334c7b
Ronie Athaydes <ronieathaydes@gmail.com>
2021-05-05 18:32:55 -0300

    Add wip, assignees and merge validations

--- Dangerfile.df.kts
+++ Dangerfile.df.kts
@@ -3,11 +3,22 @@ import systems.danger.kotlin.*
 danger(args) {
 
     onGitHub {
-        message("Thank you so much for your work here @${pullRequest.user.login} 🎉")
-        message("You might find a few suggestions from me for this Pull Request below 🙂")
+        message("Thank you so much for your work here @${pullRequest.user.login} 🎉 You might find a few suggestions from me for this Pull Request below 🙂")
+
+        if (pullRequest.title.contains("WIP", false)) {
+            fail("I noticed this is marked as Work In Progress, does it need to be open before it is ready? 🤔 If so, convert it to a Draft Pull Request and remove WIP from title 😉")
+        }
 
         if (pullRequest.body.isEmpty()) {
-            warn("You should provide a summary in the Pull Request description so that the reviewer has more context on this Pull Request 💚")
+            warn("You should provide a summary in the description so that the reviewer has more context on this Pull Request 💚")
+        }
+
+        if (pullRequest.assignees.isEmpty()) {
+            fail("This Pull Request does not have any assignees yet 🧐")
+        }
+
+        if (commits.any { githubCommit -> githubCommit.commit.message.startsWith("Merge branch '${pullRequest.base.ref}'") }) {
+            fail("Please rebase to get rid of the merge commits in this Pull Request 🙃")
         }
 
         if ((pullRequest.additions ?: 0) - (pullRequest.deletions ?: 0) > 300) {
